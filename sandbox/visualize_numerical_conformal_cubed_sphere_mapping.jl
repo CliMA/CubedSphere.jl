5b689978b65e4858b224c7346bc64cd6227f2dfc
ali.hh.ramadhan@gmail.com <ali.hh.ramadhan@gmail.com>
2021-02-26 21:04:43 -0500

    Rotate to plot full cubed sphere

--- sandbox/visualize_numerical_conformal_cubed_sphere_mapping.jl
+++ sandbox/visualize_numerical_conformal_cubed_sphere_mapping.jl
@@ -1,8 +1,10 @@
-using CubedSphere
+using Rotations
 using GLMakie
+using CubedSphere
 
-x = range(-1, 1, length=51)
-y = range(-1, 1, length=51)
+x = range(-1, 1, length=20)
+y = range(-1, 1, length=20)
+
 X = zeros(length(x), length(y))
 Y = zeros(length(x), length(y))
 Z = zeros(length(x), length(y))
@@ -11,5 +13,18 @@ for (i, x′) in enumerate(x), (j, y′) in enumerate(y)
     X[i, j], Y[i, j], Z[i, j] = CubedSphere.conformal_cubed_sphere_mapping(x′, y′)
 end
 
-wireframe(X, Y, Z)
+fig = wireframe(X, Y, Z, show_axis=false)
 
+rotations = (RotX(π/2), RotX(-π/2), RotY(π/2), RotY(-π/2), RotX(π))
+
+for R in rotations
+    X′ = similar(X)
+    Y′ = similar(Y)
+    Z′ = similar(Z)
+
+    for I in CartesianIndices(X)
+        X′[I], Y′[I], Z′[I] = R * [X[I], Y[I], Z[I]]
+    end
+
+    wireframe!(X′, Y′, Z′)
+end
