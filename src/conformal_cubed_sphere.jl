0044560f894efa00628cd527e4f3208104d0cfad
christophernhill <cnh@mit.edu>
2021-10-18 14:51:38 -0400

    Some conformal inverse bits

--- src/conformal_cubed_sphere.jl
+++ src/conformal_cubed_sphere.jl
@@ -57,4 +57,48 @@ function conformal_cubed_sphere_mapping(x, y)
     return X, Y, Z
 end
 
+"""
+   lamrofnoc_cubed_sphere_mapping(X, Y, Z)
+
+Inverse mapping for conformal cube sphere for quadrant of north-pole face in which X and Y are both
+positive. All other mappings to other cube face coordinates can be recovered from rotations of this map.
+There a 3 other quadrants for the north-pole face and five other faces for a total of twenty-four quadrants.
+Because of symmetry only the reverse for a single quadrant is needed. Because of branch cuts and the complex
+transform the inverse mappings are multi-valued in general, using a single quadrant case allows a simple
+set of rules to be applied.
+
+"""
+function lamrofnoc_cubed_sphere_mapping(X, Y, Z)
+
+        H  = Z + 1
+        Xˢ = X/H
+        Yˢ = Y/H
+        ω  = Xˢ + im*Yˢ
+
+        ra = √3 - 1
+        cb = -1 + im
+        cc = ra * cb / 2
+        ω⁰ = (ω*cb + ra)/(1-ω*cc)
+        W⁰ = im*ω⁰^3*im
+        Z  = Z_Rancic(W⁰)
+        z  = (Z^0.25)*2
+        x, y = reim(z)
+
+        kxy = abs(y) > abs(x)
+        xx = x
+        yy = y
+        !kxy && ( x = 1 - abs(yy) )
+        !kxy && ( y = 1 - abs(xx) )
+
+        xf = x
+        yf = y
+        ( X < Y ) && ( xf = y  )
+        ( X < Y ) && ( yf = x  )
+        x = xf
+        y = yf
+
+        return x, y
+end
+
 W_Rancic(Z) = sum(A_Rancic[k] * Z^(k-1) for k in 1:length(A_Rancic))
+Z_Rancic(W) = sum(B_Rancic[k] * W^(k-1) for k in 1:length(B_Rancic))
