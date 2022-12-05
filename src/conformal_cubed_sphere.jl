91f4c0a4efefaef5cb1c1872342abb3297497a68
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-12-05 17:51:36 +1100

    update docstrings

--- src/conformal_cubed_sphere.jl
+++ src/conformal_cubed_sphere.jl
@@ -2,10 +2,10 @@
     conformal_cubed_sphere_mapping(x, y)
 
 Conformal mapping of a cube onto a sphere. Maps `(x, y)` on the north-pole face of a cube
-to (X, Y, Z) coordinates in physical space. The face is oriented normal to Z-axis with
-X and Y increasing with x and y.
+to `(X, Y, Z)` coordinates in physical space. The face is oriented normal to `Z`-axis with
+`X` and `Y` increasing with `x` and `y`.
 
-The input coordinates must lie within the range -1 <= x <= 1,  -1 <= y <= 1.
+The input coordinates must lie within the range ``-1 ≤ x ≤ 1``,  ``-1 ≤ y ≤ 1``.
 
 This numerical conformal mapping is described by Rančić et al. (1996).
 
@@ -61,26 +61,27 @@ function conformal_cubed_sphere_mapping(x, y)
 end
 
 """
-   conformal_cubed_sphere_inverse_mapping(X, Y, Z)
+    conformal_cubed_sphere_inverse_mapping(X, Y, Z)
 
-Inverse mapping for conformal cube sphere for quadrant of north-pole face in which X and Y are both
+Inverse mapping for conformal cube sphere for quadrant of north-pole face in which `X` and `Y` are both
 positive. All other mappings to other cube face coordinates can be recovered from rotations of this map.
 There a 3 other quadrants for the north-pole face and five other faces for a total of twenty-four quadrants.
 Because of symmetry only the reverse for a single quadrant is needed. Because of branch cuts and the complex
 transform the inverse mappings are multi-valued in general, using a single quadrant case allows a simple
 set of rules to be applied.
 
-The mapping is valid for the cube face quadrant defined by 0 < x < 1 and 0 < y < 1, where a full cube
-face has extent -1 < x < 1 and -1 < y < 1. The quadrant for the mapping is from a cube face that has
-"north-pole" at its center (x=0,y=0) i.e has X,Y,Z = (0,0,1) at its center. The valid ranges of X and Y
-for this mapping and convention are a quadrant defined be geodesics that connect the points A, B, C and D,
-on the shell of a sphere of radius R with X,Y coordinates as follows
+The mapping is valid for the cube face quadrant defined by ``0 < x < 1`` and ``0 < y < 1``, where a full cube
+face has extent ``-1 < x < 1`` and ``-1 < y < 1``. The quadrant for the mapping is from a cube face that has
+"north-pole" at its center ``(x=0, y=0)`` i.e has `X, Y, Z = (0, 0, 1)` at its center. The valid ranges of `X`
+and `Y` for this mapping and convention are a quadrant defined be geodesics that connect the points A, B, C and D,
+on the shell of a sphere of radius ``R`` with `X`, `Y` coordinates as follows
 
-A = (0,0)
-B = (√2,0)
-C = (√3/3,√3/3)
-D = (0,√2)
-
+```
+A = (0, 0)
+B = (√2, 0)
+C = (√3/3, √3/3)
+D = (0, √2)
+```
 """
 function conformal_cubed_sphere_inverse_mapping(X, Y, Z)
     H  = Z + 1
