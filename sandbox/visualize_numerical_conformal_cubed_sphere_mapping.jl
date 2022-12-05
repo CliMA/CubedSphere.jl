640fba7b6d7f18fcb33f9235693eb17207dc3c45
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-12-06 06:03:46 +1100

    update visualization scripts

--- sandbox/visualize_numerical_conformal_cubed_sphere_mapping.jl
+++ sandbox/visualize_numerical_conformal_cubed_sphere_mapping.jl
@@ -5,25 +5,35 @@ using CubedSphere
 """
     visualize_conformal_mapping(w; x_min, y_min, x_max, y_max, n_lines, n_samples, filepath="mapping.png")
 
-Visualize a complex mapping w(z) from the rectangle `x ∈ [x_min, x_max]`, `y ∈ [y_min, y_max]` using `n_lines`
-equally spaced lines in x and y each evaluated at `n_samples` sample points.
+Visualize a complex mapping ``w(z)`` from the rectangle `x ∈ [x_min, x_max]`, `y ∈ [y_min, y_max]` using `n_lines`
+equally spaced lines in ``x`` and ``y`` each evaluated at `n_samples` sample points.
 """
 function visualize_conformal_mapping(w; x_min, y_min, x_max, y_max, n_lines, n_samples, filepath="mapping.png")
+
     z₁ = [[x + im * y for x in range(x_min, x_max, length=n_samples)] for y in range(y_min, y_max, length=n_lines)]
     z₂ = [[x + im * y for y in range(y_min, y_max, length=n_samples)] for x in range(x_min, x_max, length=n_lines)]
+
     zs = cat(z₁, z₂, dims=1)
     ws = [w.(z) for z in zs]
 
-    fig = Figure(resolution=(3840, 2160))
-    ax = fig[1, 1] = Axis(fig, xlabel="Re{w}", ylabel="Im{w}")
-    [lines!(ax, real(z), imag(z), color=RGBAf0(0, 0, 0, 0.25)) for z in zs]
-    [lines!(ax, real(w), imag(w), color="dodgerblue2") for w in ws]
-    ax.aspect = AxisAspect(1)
+    fig = Figure(resolution=(1200, 1200))
+    ax = Axis(fig[1, 1];
+              xlabel = "Re{w}",
+              ylabel = "Im{w}",
+              aspect = 1)
+
+    [lines!(ax, real(z), imag(z), color = :grey) for z in zs]
+    [lines!(ax, real(w), imag(w), color = :blue) for w in ws]
+
+    display(fig)
+
     save(filepath, fig)
 end
 
-x = range(-1, 1, length=20)
-y = range(-1, 1, length=20)
+N = 16
+
+x = range(-1, 1, length=N)
+y = range(-1, 1, length=N)
 
 X = zeros(length(x), length(y))
 Y = zeros(length(x), length(y))
@@ -33,7 +43,10 @@ for (i, x′) in enumerate(x), (j, y′) in enumerate(y)
     X[i, j], Y[i, j], Z[i, j] = conformal_cubed_sphere_mapping(x′, y′)
 end
 
-fig = wireframe(X, Y, Z, show_axis=false)
+fig = Figure()
+ax = Axis(fig[1, 1], aspect=1)
+
+wireframe!(ax, X, Y, Z, show_axis=false)
 
 rotations = (RotX(π/2), RotX(-π/2), RotY(π/2), RotY(-π/2), RotX(π))
 
