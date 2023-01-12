ea898bee112562f342e2b1662b5f269223fc626a
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-01-13 09:54:34 +1100

    better docstring + examples for conformal_cubed_sphere_mapping

--- src/conformal_cubed_sphere.jl
+++ src/conformal_cubed_sphere.jl
@@ -1,16 +1,40 @@
 """
     conformal_cubed_sphere_mapping(x, y)
 
-Conformal mapping of a cube onto a sphere. Maps `(x, y)` on the north-pole face of a cube
-to `(X, Y, Z)` coordinates in physical space. The face is oriented normal to `Z`-axis with
-`X` and `Y` increasing with `x` and `y`.
+Conformal mapping from a face of a cube onto the equivalent sector of a sphere with unit radius.
 
-The input coordinates must lie within the range ``-1 ≤ x ≤ 1``,  ``-1 ≤ y ≤ 1``.
+Map the north-pole face of a cube with coordinates `(x, y)` onto the equivalent sector of the
+sphere with coordinates `(X, Y, Z)`.
 
-This numerical conformal mapping is described by Rančić et al. (1996).
+The cube's face oriented normal to ``z``-axis and its coordinates must lie within the
+range ``-1 ≤ x ≤ 1``,  ``-1 ≤ y ≤ 1`` with its center at ``(x, y) = (0, 0)``. The coordinates
+``X, Y`` increase in the same direction as ``x, y``.
+
+The numerical conformal mapping used here is described by Rančić et al. (1996).
 
 This is a Julia translation of [MATLAB code from MITgcm](http://wwwcvs.mitgcm.org/viewvc/MITgcm/MITgcm_contrib/high_res_cube/matlab-grid-generator/map_xy2xyz.m?view=markup) that is based on
 Fortran 77 code from Jim Purser & Misha Rančić.
+
+Examples
+========
+
+The center of the cube's face ``(x, y) = (0, 0)`` is mapped onto ``(X, Y, Z) = (0, 0, 1)``
+
+```jldoctest
+julia> using CubedSphere
+
+julia> conformal_cubed_sphere_mapping(0, 0)
+(0, 0, 1.0)
+```
+
+and the edge of the cube's face at ``(x, y) = (1, 1)`` is mapped onto ``(X, Y, Z) = (√3/3, √3/3, √3/3)``
+
+```jldoctest
+julia> using CubedSphere
+
+julia> conformal_cubed_sphere_mapping(1, 1)
+(0.5773502691896256, 0.5773502691896256, 0.5773502691896257)
+```
 
 References
 ==========
