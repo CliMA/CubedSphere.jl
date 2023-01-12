40fa5e302b7b8556088cb0fd5d38365c1a312434
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-01-13 10:39:19 +1100

    add example in cartesian_to_lat_lon

--- src/cartesian_to_lat_lon.jl
+++ src/cartesian_to_lat_lon.jl
@@ -1,11 +1,24 @@
 """
     cartesian_to_lat_lon(x, y, z)
 
-Convert 3D coordinates `(x, y, z)` from the unit sphere to latitude-longitude `(lat, lon)`. Assumes `lat` is positive
+Convert 3D coordinates `(x, y, z)` on the unit sphere to latitude-longitude `(lat, lon)`. Assumes `lat` is positive
 with `z`, equatorial plane falls at ``z = 0``  and `lon` is measured anti-clockwise (eastward) from ``x``-axis (``y = 0``)
-about ``z``-axis.
+about the ``z``-axis.
 
 This is a Julia translation of [MATLAB code from MITgcm](http://wwwcvs.mitgcm.org/viewvc/MITgcm/MITgcm_contrib/high_res_cube/matlab-grid-generator/map_xyz2lonlat.m?view=markup).
+
+Examples
+========
+
+```@jlddoctest
+julia> using CubedSphere
+
+julia> cartesian_to_lat_lon(0, 0, 1)
+(90.0, 0.0)
+
+julia> cartesian_to_lat_lon(√2/4, -√2/4, √3/2)
+(59.99999999999999, -45.0)
+```
 """
 cartesian_to_lat_lon(x, y, z) = cartesian_to_latitude(x, y, z), cartesian_to_longitude(x, y, z)
 
